bdda30ef9b51714b4ba819b8572085d638c4ca1e
Jon McCartie <jon@mccartie.com>
2012-04-11 08:27:20 -0700

    Added memcache

--- main.rb
+++ main.rb
@@ -2,29 +2,47 @@ require 'rubygems'
 require "bundler/setup"
 require 'sinatra'
 require 'json'
+require 'dalli'
+require 'digest/md5'
 require 'yelpster'
+
+cache = Dalli::Client.new(nil, {expires_in: 43200}) # 12 hours
+set :cache, cache
+
+# Production-only
+configure :production do
+  require 'newrelic_rpm'
+end
 
 get '/' do
   check_param(:zipcode) 
   content_type :json
 
-  client = Yelp::Client.new
-  request = Yelp::V1::Review::Request::Location.new(
-    zipcode: params[:zipcode],
-    term: 'lunch',
-    yws_id: ENV['YWSID']
-  )
+  cache_key = Digest::MD5.hexdigest(params[:zipcode])
+  data = settings.cache.get(cache_key)
 
-  response = client.search(request)
+  if data.nil?
+    data = []
 
-  if response["message"]["text"] == "OK"
-    results = []
-    response["businesses"].map { |r| results << {name: r["name"], address: r["address1"], rating: r["avg_rating"]} }
-    return_with_callback(results.to_json, params[:callback])
-  else
-    halt 500
+    client = Yelp::Client.new
+    request = Yelp::V1::Review::Request::Location.new(
+      zipcode: params[:zipcode],
+      term: 'lunch',
+      yws_id: ENV['YWSID']
+    )
+    response = client.search(request)
+
+    if response["message"]["text"] == "OK"
+      response["businesses"].map { |r| data << {name: r["name"], address: r["address1"], rating: r["avg_rating"]} }
+    else
+      halt 500
+    end
+
+    data = data.to_json
+    settings.cache.set(cache_key, data)
   end
-  
+
+  return_with_callback(data, params[:callback])
 end
 
 
